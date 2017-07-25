17692c300a72560eb00a2802acac050fbdecaeca
Brandon Patterson <brandonnpatterson@gmail.com>
2017-07-25 15:37:57 -0500

    added about me section

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,4 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
-router.get('/schedule', (req, res) => {
-  res.render('schedule');
-});
-
-router.get('/services', (req, res) => {
-  res.render('services');
-});
-
 module.exports = router;
